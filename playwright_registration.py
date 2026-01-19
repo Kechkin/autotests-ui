5cb3363889426ee6c9fea0f9c8490792da8a0808
Kechkin <zero.echelon@mail.ru>
2026-01-19 20:02:39 +0300

    5.4 Введение в Playwright. Практика с Playwright

---
+++ playwright_registration.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.wait_for()
+    email_input.fill("user.name@gmail.com")
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.wait_for()
+    username_input.fill("username")
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.wait_for()
+    password_input.fill("password")
+
+    login_button = page.get_by_test_id('registration-page-registration-button')
+    login_button.wait_for()
+    login_button.click()
+
+    title = page.get_by_test_id("dashboard-toolbar-title-text")
+    expect(title).to_be_visible()
+
+    page.wait_for_timeout(5000)
