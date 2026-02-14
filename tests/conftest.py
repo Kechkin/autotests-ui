3ff67c84bf3341c0371c2771391db2b92b5923f1
Kechkin <zero.echelon@mail.ru>
2026-02-15 00:14:45 +0300

    6.5 Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,47 @@
-import pytest  # Импортируем pytest
-from playwright.sync_api import sync_playwright, Page
+import pytest
+from playwright.sync_api import Playwright, Page, sync_playwright
 
 
-@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def chromium_page() -> Page:  # Аннотируем возвращаемое фикстурой значение
-    # Ниже идет инициализация и открытие новой страницы
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
+
+
+@pytest.fixture(scope='session')
+def initialize_browser_state():
     with sync_playwright() as playwright:
-        # Запускаем браузер
         browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
 
-        # Передаем страницу для использования в тесте
-        yield browser.new_page()
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        # Закрываем браузер после выполнения тестов
-        browser.close()
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.wait_for()
+        email_input.fill("user.name@gmail.com")
+
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.wait_for()
+        username_input.fill("username")
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.wait_for()
+        password_input.fill("password")
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.wait_for()
+        registration_button.click()
+
+        context.storage_state(path="browser-state.json")
+
+
+@pytest.fixture(scope='function')
+def chromium_page_with_state(initialize_browser_state) -> Page:
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state="browser-state.json")
+        page = context.new_page()
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+        yield page
